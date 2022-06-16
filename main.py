0a153b4581bd3aa5a6496155e5a1278652604f0a
unknown <you@example.com>
2022-06-16 18:12:40 +0100

    im stupid

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from sys import float_repr_style
+from tkinter.messagebox import RETRY
 import pygame, random, time # importing libraries
 pygame.font.init() # initialising pygame font
 pygame.init() # initialising pygame
@@ -6,22 +8,54 @@ pygame.init() # initialising pygame
 RED = 255, 0, 0
 GREEN  = 0, 255, 0
 BLUE = 0, 0, 255
-BLACK = 255, 255, 255
-WHITE = 0, 0, 0
+WHITE = 255, 255, 255
+BLACK = 0, 0, 0
 
 # Assigning screen size variables
-SC_WIDTH, SC_LENGTH = 600, 600
+SC_WIDTH, SC_LENGTH = 500, 350
+
+# Creating Window
+WIN = pygame.display.set_mode((SC_WIDTH, SC_LENGTH))
+pygame.display.set_caption("Guess The Number Game!")
+
+TEXT_FONT = pygame.font.SysFont('comicsans', 40)
+RETRY_FONT = pygame.font.SysFont('comicsans', 40)
+
+
+# function that will draw everything on the screen
+def draw_window(user_input, retry=False, win=False):
+    WIN.fill(WHITE)
+    input_text = TEXT_FONT.render(str(user_input), 1, WHITE)
+    print(int(user_input))
+
+    box = pygame.Rect(SC_WIDTH//2-200, SC_LENGTH//2-30, 400, 60)
+
+    pygame.draw.rect(WIN, BLACK, box)
+    WIN.blit(input_text, (SC_WIDTH//2-200, SC_LENGTH//2-30))
+
+    pygame.display.update()
+
 
 # Main function
 def main():
+    user_input = ''
     
     running = True
     while running:
         for event in pygame.event.get(): 
-            if event == QUIT: # checking for quit
+            if event.type == pygame.QUIT: # checking for quit
                 running = False
-            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_0:
+                    user_input+= '0'
 
+        draw_window(user_input)
+                
+        
+
+
+
+main()
 
 
 """
